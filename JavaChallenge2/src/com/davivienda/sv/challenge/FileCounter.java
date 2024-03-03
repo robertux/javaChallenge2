6c4a7ec46ca4b632e4d905014fe6664a78fdc76c
Kevin Chacón <kevinchacon095@gmail.com>
2024-03-02 22:41:09 -0600

    Prueba Tecnica Finalizada

--- JavaChallenge2/src/com/davivienda/sv/challenge/FileCounter.java
+++ JavaChallenge2/src/com/davivienda/sv/challenge/FileCounter.java
@@ -1,7 +1,10 @@
 package com.davivienda.sv.challenge;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileCounter {
 	private String fileContent;
@@ -15,7 +18,24 @@ public class FileCounter {
 	 */
 	public int getChapterCount() {
 		//TODO: Implementar
-		return 0;
+		
+		int Contador = 0;
+		
+		try {
+			
+			//Creo la expresión regular
+			Pattern pattern = Pattern.compile("CHAPTER [IVX]+\\n", Pattern.CASE_INSENSITIVE);				
+	        Matcher matcher = pattern.matcher(fileContent);
+	        
+	        while(matcher.find())
+	        	Contador++; 
+	        
+	        return Contador;      	        
+	        
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return -1;
+		}       	
 	}
 	
 	/**
@@ -23,7 +43,24 @@ public class FileCounter {
 	 */
 	public int getDraculaMentionCount() {
 		//TODO: Implementar
-		return 0;
+		
+		int Contador=0;
+		
+		try {
+		
+			//Creo la expresión regular
+			Pattern pattern = Pattern.compile("Dracula", Pattern.CASE_INSENSITIVE);				
+	        Matcher matcher = pattern.matcher(fileContent);
+	        
+	        while(matcher.find())
+	        	Contador++; 
+	        
+	        return Contador;    
+	        
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return -1;
+		}       		
 	}
 	
 	/**
@@ -31,7 +68,60 @@ public class FileCounter {
 	 */
 	public int getBiggestChapter() {
 		//TODO: Implementar
-		return 0;
+		
+		int CapituloMasLargo = 0;
+		int LineasDelCapituloMasLargo = 0;
+		
+		int CapituloAnterior = 0;
+		int CapituloActual= 0;
+		int LineasCapituloActual = 0;
+		
+		String LineaActual = "";
+		
+		try {
+			
+			//Creo la expresión regular
+			Pattern pattern = Pattern.compile("^CHAPTER [IVX]+$", Pattern.CASE_INSENSITIVE);				
+	        Matcher matcher;
+	        
+	        //Separo el libro en lineas
+	        String[] lineas = fileContent.split("\\n");       
+	        
+	        for(int i = 0; i < lineas.length; i++) {
+	        	
+	        	LineaActual = lineas[i].trim();
+	        	
+	        	matcher = pattern.matcher(LineaActual);
+	        	
+	        	if(matcher.find() || LineaActual.equals("THE END")) { 
+	        		
+	        		CapituloAnterior = CapituloActual;
+	        		CapituloActual++;
+	        		
+	        		if(LineasCapituloActual > LineasDelCapituloMasLargo) {
+        				LineasDelCapituloMasLargo = LineasCapituloActual;
+        				CapituloMasLargo = CapituloAnterior;        				
+        			}
+	        		
+	        		LineasCapituloActual = 0;	        		
+	        		
+	        		if(LineaActual.equals("THE END")) 
+	        			break;
+	        		
+	        	}else {
+	        		
+	        		if(CapituloActual == 0) LineasCapituloActual = 0;
+	        		else LineasCapituloActual++;
+	        		
+	        	}	        	
+	        }	                
+	        	        
+	        return CapituloMasLargo;
+	        
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return -1;
+		}       				
 	}
 
 	/**
@@ -39,7 +129,32 @@ public class FileCounter {
 	 */
 	public List<String> getLetterDates() {
 		//TODO: Implementar
-		return Collections.emptyList();
+		
+		String FechaActual = "";
+		
+		try {
+			
+			//Lista de Fecha
+			List<String> Fechas = new ArrayList<>();
+			
+			//Creo la expresión regular
+			Pattern pattern = Pattern.compile("_([1-9]|[1-2][0-9]|3[0-1]) (January|February|March|April|May|June|July|August|September|October|November|December)", Pattern.CASE_INSENSITIVE);				
+	        Matcher matcher = pattern.matcher(fileContent);
+	        
+	        while (matcher.find()) {
+	        	FechaActual = matcher.group().toString().trim().substring(1); //Para quitar el _ al inicio
+	        	
+	        	if(!Fechas.contains(FechaActual))	        	
+	        		Fechas.add(FechaActual); 
+	        }
+	        
+	        return Fechas;			
+	        
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return null;
+		}
+		
 	}
 	
 	/**
@@ -47,6 +162,30 @@ public class FileCounter {
 	 */
 	public List<String> getLetterRecipients() {
 		//TODO: Implementar
-		return Collections.emptyList();
+		
+		String RemitenteActual;
+		
+		try {
+			
+			//Lista de Remitentes
+			List<String> Remitentes = new ArrayList<>();
+			
+			//Creo la expresión regular
+			Pattern pattern = Pattern.compile("_.+(Journal|Memorandum|Diary)\\._", Pattern.CASE_INSENSITIVE);				
+	        Matcher matcher = pattern.matcher(fileContent);
+	        
+	        while (matcher.find()) {
+	        	RemitenteActual = matcher.group().trim().substring(1,matcher.group().trim().length()-2); //Para quitar el _ al inicio y el ._ al final
+	        	
+	        	if(!Remitentes.contains(RemitenteActual) && !RemitenteActual.equals("Pasted in Mina Murray’s Journal"))
+	        		Remitentes.add(RemitenteActual); 
+	        }
+	        
+	        return Remitentes;			
+	        
+		}catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			return null;
+		}    			
 	}
 }
--- JavaChallenge2/src/com/davivienda/sv/challenge/FileManager.java
+++ JavaChallenge2/src/com/davivienda/sv/challenge/FileManager.java
@@ -1,5 +1,10 @@
 package com.davivienda.sv.challenge;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class FileManager {
 	public static final String FILE_URL = "https://robertux-words.s3.amazonaws.com/pg345.txt";
 	
@@ -9,6 +14,34 @@ public class FileManager {
 	 */
 	public String loadFile() {
 		//TODO: Implementar un cliente HTTP para cargar en memoria el archivo ubicado en la URL
-		return null;
+		
+		try {
+			
+			// Crear objeto URL
+            URL url = new URL(FILE_URL);
+
+			//Abrir la conexión
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            //Se configura la conexión con el metodo GET
+            con.setRequestMethod("GET");
+
+            //Recolectamos el libro entero
+            BufferedReader BR = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            
+            String Linea;
+            StringBuilder SB = new StringBuilder();
+
+            while ((Linea = BR.readLine()) != null) {
+            	SB.append(Linea);
+            	SB.append("\n");
+            }
+            BR.close();
+            
+            return SB.toString();
+        } catch (Exception ex) {
+        	System.out.println(ex.getMessage());
+        	return null;
+        }		
 	}
 }
