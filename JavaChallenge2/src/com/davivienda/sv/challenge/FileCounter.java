84ef436afb0445b41490b1bf05fe3b1c11bcd136
Manuel <20605851+ManuelNolasco@users.noreply.github.com>
2024-03-11 12:04:48 -0600

    Ligeras mejoras
    Ligeras mejoras como removido código innecesario, cambio de algunas variables a unas más descriptivas

--- JavaChallenge2/src/com/davivienda/sv/challenge/FileCounter.java
+++ JavaChallenge2/src/com/davivienda/sv/challenge/FileCounter.java
@@ -10,6 +10,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class FileCounter {
+	private static final String CONTENIDO_DESCARTAR = "Contents";
+	private static final String CONTENIDO_FIN_DESCARTAR = "How these papers";
+	private static final String CAPITULOS_DESCARTAR = "of knowledge of those";
+	private static final String FIN_CAPITULOS_DESCARTAR = "This then was the Un-Dead";
+	private static final String INICIO_CAPITULO = "CHAPTER";
+	
 	private String fileContent;
 	
 	public FileCounter(String content) {
@@ -28,20 +34,20 @@ public class FileCounter {
 				InputStream inputStream = con.getInputStream();
 				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 				String linea = br.readLine();
-				
 				String cadena = "";
-				int bandera=0;
+				boolean continua = false;
 				while(linea != null) {
-					if(linea.contains("How these papers")) break;//FIN
-					if(linea.contains("Contents") || bandera == 1) {//INICIO
-						bandera = 1;
-						if(linea.contains("Contents")) linea = br.readLine();
+					if(linea.contains(CONTENIDO_FIN_DESCARTAR)) break;//FIN
+					if(linea.contains(CONTENIDO_DESCARTAR) || continua) {//INICIO
+						continua = true;
+						if(linea.contains(CONTENIDO_DESCARTAR)) linea = br.readLine();
 						cadena += linea + "\n";
 					}					
 					linea = br.readLine();
 				}
-				cadena = cadena.trim();//LIMPIA
+				//cadena = cadena.trim();//LIMPIA
 				conteo = cadena.split("CHAPTER").length-1;//Cuenta las veces que partio el segmento tomado
+				cadena = "";
 			}
 			con.disconnect();
 		} catch (Exception e) {
@@ -62,13 +68,13 @@ public class FileCounter {
 				InputStream inputStream = con.getInputStream();
 				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 				String linea = br.readLine();
-				
 				String cadena = "";
 				while(linea != null) {
 					cadena += linea + "\n";
 					linea = br.readLine();
 				}
 				conteo = cadena.toUpperCase().split("DRACULA").length-1;//Cuenta las veces que partio el segmento tomado
+				cadena = "";
 			}
 			con.disconnect();
 		} catch (Exception e) {
@@ -88,41 +94,36 @@ public class FileCounter {
 			if(con.getResponseCode() == 200) {
 				InputStream inputStream = con.getInputStream();
 				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-				String linea = br.readLine();
-
-				String CONTENIDO_BUSCAR = "of knowledge of those";
-				String FIN_CAPITULOS = "This then was the Un-Dead";
-				String cadena = "";
+				String linea = br.readLine();				
 				//INTERPRETACION ACTUAL
 				//INCLUYE DEL CAPITULO EL NOMBRE, TITULO Y ULTIMAS LINEAS ANTES DEL SIGUIENTE CAPITULO
 				//TAMBIEN ESPACIOS Y SALTOS DE LINEA DENTRO DE CADA CAPITULO, MENOS LOS SALTOS DE LOS EXTREMOS
 
-				int bandera = 0;
 				int veces = 0;
+				String texto = "";
+				boolean continua = false;
 				List<Integer> lista = new ArrayList<Integer>();
 
 				while(linea != null) {
-					if((linea.contains("CHAPTER") && veces == 1) || linea.contains(FIN_CAPITULOS)) {
+					if((linea.contains(INICIO_CAPITULO) && veces == 1) || linea.contains(FIN_CAPITULOS_DESCARTAR)) {
 						//GUARDAR EL DATO EN LISTA, REINCIAR VECES, CADENA Y NO SALIR,
 						//ES DECIR CONTINUAR RECORRIENDO EL ARCHIVO PARA MÁS ENCONTRAR MAS CAPITULOS
-						lista.add(cadena.trim().length());
-						if(linea.contains(FIN_CAPITULOS))
-							break;						
+						lista.add(texto.trim().length());					
 						veces = 0;
-						cadena = "";
+						texto = "";
+						if(linea.contains(FIN_CAPITULOS_DESCARTAR)) break;
 					}//FIN
-					if(linea.contains(CONTENIDO_BUSCAR) || bandera == 1) {//INICIO
-						bandera = 1;
-						if(linea.contains(CONTENIDO_BUSCAR)) linea = br.readLine();
-						if(linea.contains("CHAPTER") && veces != 1) {
-							cadena = linea + "\n";
-							veces++;
+					if(linea.contains(CAPITULOS_DESCARTAR) || continua) {//INICIO
+						continua = true;
+						if(linea.contains(CAPITULOS_DESCARTAR)) linea = br.readLine();
+						if(linea.contains(INICIO_CAPITULO) && veces == 0) {
+							texto = linea + "\n";
+							veces = 1;
 						} else
-							cadena += linea + "\n";
+							texto += linea + "\n";
 					}
 					linea = br.readLine();
 				}
-				cadena = cadena.trim();//LIMPIA
 				for (Integer num : lista) {
 					if(num > mayor) { 
 						mayor = num;
@@ -136,7 +137,7 @@ public class FileCounter {
 		return mayor;
 	}
 
-	/**
+	/**Lista de fechas
 	 * @return Retorna la lista de fechas en las cartas, diarios y memorandums mencionados en le libro, en formato String tal cual como aparecen en el libro. Ej. 30 October, 4 November, ... 
 	 */
 	public List<String> getLetterDates() {
@@ -148,43 +149,26 @@ public class FileCounter {
 				InputStream inputStream = con.getInputStream();
 				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 				String linea = br.readLine();
-
-				String CONTENIDO_BUSCAR = "of knowledge of those";
-				String FIN_CAPITULOS = "This then was the Un-Dead";
-				String cadena = "";
-				//INTERPRETACION ACTUAL
-				//ESTADO DEL METODO POR EL MOMENTO, MEJORABLE
-
-				int bandera = 0;
+				//METODO MEJORABLE
+				boolean continua = false;
 				String[] meses = { "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
 						"JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER" };
-				String cadenaFormato = "";
-
 				while(linea != null) {
-					cadenaFormato = linea.toUpperCase();
-					for (String mes : meses) {
-				        if (cadenaFormato.contains(mes)) {
-				        	if(linea.contains("_") && linea.contains("._"))
-								lista.add(linea.substring(linea.indexOf("_"), linea.lastIndexOf("_")).replace("_", "").replace(".", ""));
-				            break;
-				        }
-				    }
-
-					if(linea.contains(FIN_CAPITULOS))
-						break;//FIN
-					
-					if(linea.contains(CONTENIDO_BUSCAR) || bandera == 1) {//INICIO
-						bandera = 1;
-						if(linea.contains(CONTENIDO_BUSCAR)) linea = br.readLine();
-						if(linea.trim().equals("CHAPTER I")) {
-							cadena = linea + "\n";
-						} else
-							cadena += linea + "\n";
+					if(linea.contains(FIN_CAPITULOS_DESCARTAR)) break;//FIN
+					if(linea.contains(CAPITULOS_DESCARTAR) || continua) {//INICIO
+						continua = true;
+						if(linea.contains(CAPITULOS_DESCARTAR)) linea = br.readLine();
+						for (String mes : meses) {
+					        if (linea.toUpperCase().contains(mes)) {
+					        	if(linea.contains("_") && linea.contains("._"))
+									lista.add(linea.substring(linea.indexOf("_")+1, linea.lastIndexOf("_")-1));//.replace("_", "").replace(".", ""));
+					        	//ACA MEJORAS
+					            break;
+					        }
+					    }
 					}
-
 					linea = br.readLine();
 				}
-				//cadena = cadena.trim();//LIMPIA
 			}
 			con.disconnect();
 		} catch (Exception e) {
@@ -193,7 +177,7 @@ public class FileCounter {
 		return lista;
 	}
 	
-	/**
+	/**Lista de remitentes
 	 * @return Retorna la lista de remitentes de las cartas, diarios y memorandums mencoinados en el libro, en formato String tal cual como aparece en el libro. Ej. Jonathan Harker’s Journal, Dr. Van Helsing’s Memorandum, Dr. Seward’s Diary...
 	 */
 	public List<String> getLetterRecipients() {
@@ -206,20 +190,18 @@ public class FileCounter {
 				BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 				String linea = br.readLine();
 				//METODO MEJORABLE
-				String cadena = "";
-				int bandera=0;
+				boolean continua = false;
 				while(linea != null) {
-					if(linea.contains("How these papers")) break;//FIN
-					if(linea.contains("Contents") || bandera == 1) {//INICIO
-						bandera = 1;
-						if(linea.contains("Contents")) linea = br.readLine();
-						cadena += linea + "\n";
+					if(linea.contains(CONTENIDO_FIN_DESCARTAR)) break;//FIN
+					if(linea.contains(CONTENIDO_DESCARTAR) || continua) {//INICIO
+						continua = true;
+						if(linea.contains(CONTENIDO_DESCARTAR)) linea = br.readLine();
 						if(linea.contains(". ") && linea.contains("’s"))
-							lista.add(linea.substring(linea.indexOf(". "), linea.lastIndexOf("’s")).replace(". ", "").replace("’s", ""));
+							lista.add(linea.substring(linea.indexOf(". ")+2, linea.lastIndexOf("’s")-2));//.replace(". ", "").replace("’s", ""));
+						//ACA MEJORAS
 					}					
 					linea = br.readLine();
 				}
-				cadena = cadena.trim();//LIMPIA
 			}
 			con.disconnect();
 		} catch (Exception e) {
